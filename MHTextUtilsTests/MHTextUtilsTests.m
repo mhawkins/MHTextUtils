5a9f762b68cbd90a5784daebe19d780d025d4900
Matt Hawkins <matthew.b.hawkins@gmail.com>
2013-06-29 11:14:35 -0400

    renamed methods to match Apple style 'object by method'. added MIT license

--- MHTextUtilsTests/MHTextUtilsTests.m
+++ MHTextUtilsTests/MHTextUtilsTests.m
@@ -5,6 +5,15 @@
 //  Created by Matt Hawkins on 6/29/13.
 //  Copyright (c) 2013 Matt Hawkins. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+//  to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "MHTextUtilsTests.h"
 #import "NSString+MHTextUtils.h"
@@ -31,7 +40,7 @@
 - (void)testTruncatingLongStringThatEndsWithNonWhitespaceCharacterAndWantsAnEllipsis
 {
     NSString *sampleString = @"lorem ipsum dolar";
-    NSString *result = [sampleString stringTruncatedToLength:10 withEllipsis:YES];
+    NSString *result = [sampleString stringByTruncatingToFitLength:10 withEllipsis:YES];
     
     // Verify strings are equal
     STAssertTrue([result isEqualToString:@"lorem i..."], @"Truncated strings are not equal.");
@@ -40,7 +49,7 @@
 -(void)testTruncatingLongStringThatEndsWithWhitespaceCharacterAndWantsAnEllipsis
 {
     NSString *sampleString = @"lorem ipsum dolar";
-    NSString *result = [sampleString stringTruncatedToLength:9 withEllipsis:YES];
+    NSString *result = [sampleString stringByTruncatingToFitLength:9 withEllipsis:YES];
     
     STAssertTrue([result isEqualToString:@"lorem "], @"Truncated strings are not equal.");
 }
@@ -48,7 +57,7 @@
 - (void)testTruncatingLongStringThatEndsWithNonWhitespaceCharacterAndDoesNotWantAnEllipsis
 {
     NSString *sampleString = @"lorem ipsum dolar";
-    NSString *result = [sampleString stringTruncatedToLength:10 withEllipsis:NO];
+    NSString *result = [sampleString stringByTruncatingToFitLength:10 withEllipsis:NO];
     
     STAssertTrue([result isEqualToString:@"lorem ipsu"], @"Truncated strings are not equal.");
 }
@@ -56,7 +65,7 @@
 -(void)testTruncatingLongStringThatEndsWithWhitespaceCharacterAndDoesNotWantAnEllipsis
 {
     NSString *sampleString = @"lorem ipsum dolar";
-    NSString *result = [sampleString stringTruncatedToLength:9 withEllipsis:NO];
+    NSString *result = [sampleString stringByTruncatingToFitLength:9 withEllipsis:NO];
     
     STAssertTrue([result isEqualToString:@"lorem ips"], @"Truncated strings are not equal.");
 }
@@ -64,7 +73,7 @@
 - (void)testTruncatingLongStringThatEndsWithNonWhitespaceCharacter
 {
     NSString *sampleString = @"lorem ipsum dolar";
-    NSString *result = [sampleString stringTruncatedToLength:10];
+    NSString *result = [sampleString stringByTruncatingToFitLength:10];
     
     // Verify strings are equal
     STAssertTrue([result isEqualToString:@"lorem i..."], @"Truncated strings are not equal.");
@@ -73,9 +82,28 @@
 -(void)testTruncatingLongStringThatEndsWithWhitespaceCharacter
 {
     NSString *sampleString = @"lorem ipsum dolar";
-    NSString *result = [sampleString stringTruncatedToLength:9];
+    NSString *result = [sampleString stringByTruncatingToFitLength:9];
     
     STAssertTrue([result isEqualToString:@"lorem "], @"Truncated strings are not equal.");
 }
+
+- (void)testTruncatingShortStringThatEndsWithNonWhitespaceCharacterAndWantsAnEllipsis
+{
+    NSString *sampleString = @"lorem";
+    NSString *result = [sampleString stringByTruncatingToFitLength:5 withEllipsis:YES];
+    
+    // Verify strings are equal
+    STAssertTrue([result isEqualToString:@"lorem"], @"Truncated strings are not equal.");
+}
+
+- (void)testTruncatingShortStringThatEndsWithNonWhitespaceCharacter
+{
+    NSString *sampleString = @"lorem";
+    NSString *result = [sampleString stringByTruncatingToFitLength:5];
+    
+    // Verify strings are equal
+    STAssertTrue([result isEqualToString:@"lorem"], @"Truncated strings are not equal.");
+}
+
 
 @end
--- README.md
+++ README.md
@@ -2,3 +2,25 @@ MHTextUtils
 ===========
 
 Simple text truncating library for NSString.
+
+Description
+------------------
+
+This is a dirt simple, unimpressive text truncating library for NSString. I had ran into a project recently where paragraphs of text were being passed into a UITableViewCell spiking its memory footprint. This project is a just quick way for dropping the memory usage.
+
+
+Usage
+------------------
+
+    import "NSString+MHTextUtils.h"
+
+    NSString *shortenedString = [@"lorem ipsum dolar" stringByTruncatingToFitLength:9];
+    // lorem i...
+    
+    
+Adding NSString+MHTextUtils to your project
+------------------    
+
+1. Copy NSString+MHTextUtils.h and NSString+MHTextUtils.m into your project.
+2. Import the category 
+    import "NSString+MHTextUtils.h"
